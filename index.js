67487f46af96c196e249a9718566bf216bf6ba45
kibutan <s1170230@gmail.com>
2022-06-11 23:24:54 +0900

    最高か

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.on("ready", (client) => {
 
 client.on("messageCreate", (message) => {
   if (message.content === "ping") {
-    message.reply("github connected").catch(console.error);
+    message.reply("最高か").catch(console.error);
   }
 });
 
