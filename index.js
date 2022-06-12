5651878062d2ff5bbe1317c41adc0a9247b50304
kibutan <s1170230@gmail.com>
2022-06-12 11:17:37 +0900

    Delete

--- index.js
+++ index.js
@@ -8,25 +8,13 @@ const isPing = (msg) => {
   return false;
 };
 
-const isTime = (msg) => {
-  if (/Time/gi.test(msg.content)) return true;
-  return false;
-};
-
 client.on("ready", (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("messageCreate", (message) => {
-  // if (message.content === "ping") {
-  // message.reply("最高か").catch(console.error);
-  // }
   if (isPing(message)) {
     message.reply("最高か").catch(console.error);
-  }
-  if (isTime(message)) {
-    date = new Date();
-    message.replay(String(date));
   }
 });
 
