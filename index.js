630b712eeb3846341ec6e2d3b73924a3e77546bd
kibutan <s1170230@gmail.com>
2022-06-15 22:03:38 +0900

    dyno on wasureteta

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on("messageCreate", (message) => {
   }
 });
 
-cron.schedule("* 13 * * *", () => {
+cron.schedule("30 13 * * *", () => {
   client.channels.cache
     .get("881408091986481162")
     .send(
