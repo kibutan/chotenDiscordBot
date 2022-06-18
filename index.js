326f15b0b7476ffe16e30dd8d326e072ed577775
kibutan <s1170230@gmail.com>
2022-06-18 21:12:18 +0900

    hogescadsadv

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const fs = require("node:fs");
-const path = require("node:path");
 const Discord = require("discord.js");
 const cron = require("node-cron");
 const dotenv = require("dotenv");
@@ -8,19 +6,6 @@ dotenv.config();
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
 });
-
-client.commands = new Discord.Collection();
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
-
-for (const file of commandFiles) {
-  console.log("file", file);
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
-}
 
 const isPing = (msg) => {
   if (/ping/gi.test(msg.content)) return true;
@@ -52,7 +37,7 @@ client.on("messageCreate", (message) => {
 // UTC13時 = 日本時間22時
 cron.schedule("0 0 13 * * *", () => {
   client.channels.cache
-    .get(process.env.BOT_CHANNEL_ID)
+    .get("881408091986481162")
     .send(
       "ジェルばんは！\n" +
         "あなたのインターネット・エンジェル超てんちゃんだよ💖\n" +
