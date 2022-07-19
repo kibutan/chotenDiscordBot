9d509e3a0822d69a41d718b4ce768eb3cdb82fd1
kibutan <s1170230@gmail.com>
2022-07-19 21:35:02 +0900

    remove axios

--- commands/cat.js
+++ commands/cat.js
@@ -9,11 +9,6 @@ async function getJSONResponse(body) {
   return JSON.parse(fullBody);
 }
 
-const statusChange = {
-  status: "ストレス",
-  value: -1,
-};
-
 module.exports = {
   data: new SlashCommandBuilder().setName("cat").setDescription("random cat"),
   async execute(interaction) {
@@ -23,9 +18,5 @@ module.exports = {
     const res = await getJSONResponse(catResult.body);
     const { url } = res[0];
     await interaction.reply({ files: [url] });
-    await axios.post(
-      "https://script.google.com/macros/s/AKfycbwMZ8P5jt7O9rQJugO5IfQYLrSRWT04uviLI5o3lngVazr5EDjcnPOaoY2w_05m6Vct/exec",
-      statusChange
-    );
   },
 };
