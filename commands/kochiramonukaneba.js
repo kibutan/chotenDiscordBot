d74adb4453610b42f063cf47ab20a86c3e95770f
kibutan <s1170230@gmail.com>
2023-01-17 18:53:45 +0900

    doppo_nandaatemee

--- commands/kochiramonukaneba.js
+++ commands/kochiramonukaneba.js
@@ -2,12 +2,12 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("doppo_nandaatemee")
-    .setDescription("なんだァ？てめェ……"),
+    .setName("kochiramonukaneba")
+    .setDescription("此方も抜かねば… 無作法というもの…"),
   async execute(interaction) {
     await interaction.reply({
       files: [
-        "https://cdn.discordapp.com/attachments/881408091986481162/1063824459460526203/IMG_0409.jpg",
+        "https://cdn.discordapp.com/attachments/881408091986481162/1063824282087600198/IMG_0408.jpg",
       ],
     });
   },
