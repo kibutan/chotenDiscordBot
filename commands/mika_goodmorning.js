d34e2924b325b2f6bf0001f9fc3078de60a7f604
kibutan <s1170230@gmail.com>
2023-02-05 14:57:12 +0900

    mika_goodmorning

--- commands/mika_goodmorning.js
+++ commands/mika_goodmorning.js
@@ -2,12 +2,12 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("hitsuyoudattamono")
-    .setDescription("顧客が本当に必要だったもの"),
+    .setName("mika_goodmorning")
+    .setDescription("目覚めるミカ"),
   async execute(interaction) {
     await interaction.reply({
       files: [
-        "https://cdn.discordapp.com/attachments/881408091986481162/1055122634817409024/project_comedy_l.jpg",
+        "https://cdn.discordapp.com/attachments/881408091986481162/1071669699508715591/104724336_1.gif",
       ],
     });
   },
