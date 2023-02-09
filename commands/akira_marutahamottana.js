3f626c6ce1f304cb77cb671b467dbe738cac47a5
kibutan <s1170230@gmail.com>
2023-02-10 07:12:07 +0900

    akira_marutahamottana

--- commands/akira_marutahamottana.js
+++ commands/akira_marutahamottana.js
@@ -2,12 +2,12 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("hiroyuki_deetanankanexeyo")
-    .setDescription("データなんかねぇよ"),
+    .setName("akira_marutahamottana")
+    .setDescription("みんな丸太は持ったな!! 行くぞォ!!"),
   async execute(interaction) {
     await interaction.reply({
       files: [
-        "https://cdn.discordapp.com/attachments/881408091986481162/1056508385211732008/41087414.png",
+        "https://cdn.discordapp.com/attachments/1066922904131997797/1073234035410743317/Z.png",
       ],
     });
   },
