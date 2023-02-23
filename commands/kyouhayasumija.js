9a67f38f7a3d36a8b19f66276099682d56c55b48
kibutan <s1170230@gmail.com>
2023-02-23 10:38:22 +0900

    kyouhayasumija

--- commands/kyouhayasumija.js
+++ commands/kyouhayasumija.js
@@ -2,12 +2,12 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("gosentyouenhoshii")
-    .setDescription("5000兆円欲しい!"),
+    .setName("kyouhayasumija")
+    .setDescription("今日は休みじゃ グフフ"),
   async execute(interaction) {
     await interaction.reply({
       files: [
-        "https://cdn.discordapp.com/attachments/881408091986481162/1070995155127119933/0.png",
+        "https://cdn.discordapp.com/attachments/881408091986481162/1078128154243248260/71193aecf59ed8757820ffa28e703d69.png",
       ],
     });
   },
