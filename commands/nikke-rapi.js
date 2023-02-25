0cf01777c56949b66612c8e34c3209e08334c1f7
kibutan <s1170230@gmail.com>
2023-02-25 23:45:06 +0900

    mattekuretamae

--- commands/nikke-rapi.js
+++ commands/nikke-rapi.js
@@ -2,12 +2,14 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("nikke_rapi")
-    .setDescription("火力集中…！"),
+    .setName("mattekuretamae")
+    .setDescription(
+      "待ってくれたまえ 言葉の洪水をワッと いっきにあびせかけるのは！"
+    ),
   async execute(interaction) {
     await interaction.reply({
       files: [
-        "https://cdn.discordapp.com/attachments/986208932831825950/1038658412059299840/1667667018613.png",
+        "https://cdn.discordapp.com/attachments/848755299513925674/1077957499019345980/IMG_0478.png",
       ],
     });
   },
