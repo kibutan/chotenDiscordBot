a2c605339b617d65ad9fe0e7be3cabee807457d6
kibutan <s1170230@gmail.com>
2023-03-12 08:35:21 +0900

    dr_k_imanaradekimasu

--- commands/dr_k_imanaradekimasu.js
+++ commands/dr_k_imanaradekimasu.js
@@ -2,12 +2,12 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("silencesuzuka_yoroshiku")
-    .setDescription("夜露死苦…"),
+    .setName("dr_k_imanaradekimasu")
+    .setDescription("いえ…… 今ならできます！(ｷﾞｭｯ"),
   async execute(interaction) {
     await interaction.reply({
       files: [
-        "https://cdn.discordapp.com/attachments/881408091986481162/1054729021591654440/IMG_7969.png",
+        "https://cdn.discordapp.com/attachments/881408091986481162/1084105132272275536/IMG_8500.png",
       ],
     });
   },
